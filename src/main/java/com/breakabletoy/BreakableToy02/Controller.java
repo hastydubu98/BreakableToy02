ec0d7a89a50694dc8196a8d02a06eb5f795c59cc
Andrés Emmanuel González Sánchez <andres.gonzalez@encora.com>
2025-02-23 18:34:24 -0600

    Created endpoint for getting single artist

--- src/main/java/com/breakabletoy/BreakableToy02/Controller.java
+++ src/main/java/com/breakabletoy/BreakableToy02/Controller.java
@@ -91,4 +91,29 @@ public class Controller {
         }
         return spotifyService.getTopArtists(accessToken);
     }
+
+    @GetMapping("/spotify/artist/{id}")
+    public ResponseEntity<String> getArtistInfo(@PathVariable String id) {
+
+        // Spotify API endpoint to get artist details
+        String url = "https://api.spotify.com/v1/artists/" + id;
+
+        String accessToken = tokenStorage.getAccessToken();
+
+        // Set up the authorization header with the access token
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);  // Include the Bearer token
+
+        // Set up the HTTP entity with the authorization header
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        // Create a RestTemplate to make the HTTP request
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Send the GET request to Spotify API to retrieve the artist's information
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+        // Return the response body
+        return response;
+    }
 }
